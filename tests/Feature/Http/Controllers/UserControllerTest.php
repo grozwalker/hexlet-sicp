79c0a9ff7e589d4a7fbad5422242790a935c96e2
Andrey Groza <andrey.groza@mail.ru>
2019-09-14 09:59:04 +0300

    test

--- tests/Feature/Http/Controllers/UserControllerTest.php
+++ tests/Feature/Http/Controllers/UserControllerTest.php
@@ -3,12 +3,31 @@
 namespace Tests\Feature\Http\Controllers;
 
 use App\User;
+use Illuminate\Database\Schema\Blueprint;
+use Schema;
 use Tests\TestCase;
 
 class UserControllerTest extends TestCase
 {
     public function testShow()
     {
+        Schema::create('read_chapters', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('chapter_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->timestamps();
+
+            $table->unique(['chapter_id', 'user_id']);
+
+            $table->foreign('chapter_id')
+                ->references('id')
+                ->on('chapters');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+
         $user = factory(User::class)->create();
 
         $this->actingAs($user);
